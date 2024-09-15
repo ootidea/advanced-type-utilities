f0f04832dd135d87075e1e14d54544ef99f2d31a
ootidea <ootidea@gmail.com>
2024-09-15 16:06:25 +0900

    feat: define IsOneOf type

---
+++ src/Type level predicate/IsOneOf.ts
@@ -0,0 +1,15 @@
+import { assertTypeEquality } from '../assertTypeEquality'
+import type { Equals } from './Equals'
+
+export type IsOneOf<T, U extends readonly unknown[]> = U extends readonly [infer H, ...infer L]
+  ? Equals<T, H> extends true
+    ? true
+    : IsOneOf<T, L>
+  : false
+
+assertTypeEquality<IsOneOf<string, [string, number]>, true>()
+assertTypeEquality<IsOneOf<string, [number, bigint]>, false>()
+assertTypeEquality<IsOneOf<string, [any, unknown, never]>, false>()
+assertTypeEquality<IsOneOf<string, [string | number]>, false>()
+assertTypeEquality<IsOneOf<'text', [string]>, false>()
+assertTypeEquality<IsOneOf<string, []>, false>()
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export type { TypedArray } from './type-alias/TypedArray'
 export type { MergeIntersection } from './object-manipulation/MergeIntersection'
 
 export type { Equals } from './Type level predicate/Equals'
+export type { IsOneOf } from './Type level predicate/IsOneOf'
