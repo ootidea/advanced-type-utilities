19a28aa4ab0a0a2ca6bab5b2b248de6225e05815
ootidea <ootidea@gmail.com>
2024-09-22 12:15:35 +0900

    feat: define NumberOrderRankOf type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-export type { Digit } from './type-alias/Digit'
-export type { Infinity } from './type-alias/Infinity'
-export type { NegativeInfinity } from './type-alias/NegativeInfinity'
-export type { nullish } from './type-alias/nullish'
-export type { TypedArray } from './type-alias/TypedArray'
+export type { FixedLengthArray } from './array-generation/FixedLengthArray'
+
+export type { NumberOrderRankOf } from './number-processing/NumberOrderRankOf'
 
 export type { MakeOptionalPropertiesAcceptUndefined } from './object-manipulation/MakeOptionalPropertiesAcceptUndefined'
 export type { MergeIntersection } from './object-manipulation/MergeIntersection'
 export type { OptionalKeyOf } from './object-manipulation/OptionalKeyOf'
 export type { ValueOf } from './object-manipulation/ValueOf'
+
+export type { Digit } from './type-alias/Digit'
+export type { Infinity } from './type-alias/Infinity'
+export type { NegativeInfinity } from './type-alias/NegativeInfinity'
+export type { nullish } from './type-alias/nullish'
+export type { TypedArray } from './type-alias/TypedArray'
 
 export type { Equals } from './Type level predicate/Equals'
 export type { IsOneOf } from './Type level predicate/IsOneOf'
---
+++ src/number-processing/NumberOrderRankOf.ts
@@ -0,0 +1,60 @@
+import type { IsOneOf } from '../Type level predicate/IsOneOf'
+import { assertTypeEquality, it } from '../assertTypeEquality'
+import type { Infinity } from '../type-alias/Infinity'
+import type { NegativeInfinity } from '../type-alias/NegativeInfinity'
+
+/**
+ * 9: Infinity
+ * 8: Positive number with positive exponent (e.g., 1e+30)
+ * 7: Positive number without exponent
+ * 6: Positive number with negative exponent (e.g., 1e-30)
+ * 5: Zero
+ * 4: Negative number with negative exponent (e.g., -1e-30)
+ * 3: Negative number without exponent
+ * 2: Negative number with positive exponent (e.g., -1e+30)
+ * 1: -Infinity
+ */
+export type NumberOrderRankOf<T extends number> = T extends T
+  ? IsOneOf<T, [number, any]> extends true
+    ? 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+    : T extends NegativeInfinity
+      ? 1
+      : `${T}` extends `-${string}e+${string}`
+        ? 2
+        : `${T}` extends `-${string}e-${string}`
+          ? 4
+          : `${T}` extends `-${string}`
+            ? 3
+            : T extends 0
+              ? 5
+              : T extends Infinity
+                ? 9
+                : `${T}` extends `${string}e+${string}`
+                  ? 8
+                  : `${T}` extends `${string}e-${string}`
+                    ? 6
+                    : 7
+  : never
+
+it('returns the order rank of a number literal type', () => {
+  assertTypeEquality<NumberOrderRankOf<Infinity>, 9>()
+  assertTypeEquality<NumberOrderRankOf<1e30>, 8>()
+  assertTypeEquality<NumberOrderRankOf<9.5e30>, 8>()
+  assertTypeEquality<NumberOrderRankOf<1>, 7>()
+  assertTypeEquality<NumberOrderRankOf<0.5>, 7>()
+  assertTypeEquality<NumberOrderRankOf<1e-30>, 6>()
+  assertTypeEquality<NumberOrderRankOf<9.5e-30>, 6>()
+  assertTypeEquality<NumberOrderRankOf<0>, 5>()
+  assertTypeEquality<NumberOrderRankOf<-1e-30>, 4>()
+  assertTypeEquality<NumberOrderRankOf<-9.5e-30>, 4>()
+  assertTypeEquality<NumberOrderRankOf<-1>, 3>()
+  assertTypeEquality<NumberOrderRankOf<-0.5>, 3>()
+  assertTypeEquality<NumberOrderRankOf<-1e30>, 2>()
+  assertTypeEquality<NumberOrderRankOf<-9.5e30>, 2>()
+  assertTypeEquality<NumberOrderRankOf<NegativeInfinity>, 1>()
+})
+it('distributes over union types', () => {
+  assertTypeEquality<NumberOrderRankOf<-1 | 0 | 1>, 3 | 5 | 7>()
+  assertTypeEquality<NumberOrderRankOf<number>, 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9>()
+  assertTypeEquality<NumberOrderRankOf<any>, 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9>()
+})
