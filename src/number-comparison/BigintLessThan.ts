bf83490ebfe187296cf37f15c01d35fb1bd4b0ce
ootidea <ootidea@gmail.com>
2024-09-25 19:28:44 +0900

    feat: BigintLessThan distributes

--- src/number-comparison/BigintLessThan.ts
+++ src/number-comparison/BigintLessThan.ts
@@ -1,23 +1,41 @@
 import { assertTypeEquality, it, test } from '../testUtilities'
 import type { DigitsLessThan } from './DigitArrayLessThan'
 
-export type BigintLessThan<N extends bigint, M extends bigint> = `${N}` extends `-${infer NP}`
-  ? `${M}` extends `-${infer MP}`
-    ? DigitsLessThan<MP, NP>
-    : true
-  : `${M}` extends `-${string}`
-    ? false
-    : DigitsLessThan<`${N}`, `${M}`>
+export type BigintLessThan<N extends bigint, M extends bigint> = N extends N
+  ? M extends M
+    ? bigint extends N
+      ? boolean
+      : bigint extends M
+        ? boolean
+        : `${N}` extends `-${infer NP}`
+          ? `${M}` extends `-${infer MP}`
+            ? DigitsLessThan<MP, NP>
+            : true
+          : `${M}` extends `-${string}`
+            ? false
+            : DigitsLessThan<`${N}`, `${M}`>
+    : never
+  : never
 
 test('positive-positive', () => {
   assertTypeEquality<BigintLessThan<2n, 6n>, true>()
+  assertTypeEquality<BigintLessThan<2n, 2n>, false>()
+  assertTypeEquality<BigintLessThan<6n, 2n>, false>()
   assertTypeEquality<BigintLessThan<123n, 50n>, false>()
   assertTypeEquality<BigintLessThan<50n, 123n>, true>()
+  assertTypeEquality<BigintLessThan<19n, 55n>, true>()
+  assertTypeEquality<BigintLessThan<55n, 19n>, false>()
+  assertTypeEquality<BigintLessThan<55n, 55n>, false>()
+  assertTypeEquality<BigintLessThan<55n, 56n>, true>()
 })
 test('negative-negative', () => {
-  assertTypeEquality<BigintLessThan<-2n, -6n>, false>()
   assertTypeEquality<BigintLessThan<-6n, -2n>, true>()
   assertTypeEquality<BigintLessThan<-2n, -2n>, false>()
+  assertTypeEquality<BigintLessThan<-2n, -6n>, false>()
+  assertTypeEquality<BigintLessThan<-123n, -50n>, true>()
+  assertTypeEquality<BigintLessThan<-50n, -123n>, false>()
+  assertTypeEquality<BigintLessThan<-55n, -55n>, false>()
+  assertTypeEquality<BigintLessThan<-55n, -54n>, true>()
 })
 test('positive-negative', () => {
   assertTypeEquality<BigintLessThan<2n, -6n>, false>()
@@ -39,4 +57,15 @@ it('compares the number of digits', () => {
   assertTypeEquality<BigintLessThan<1234567890n, 0n>, false>()
   assertTypeEquality<BigintLessThan<-1n, -1234567890n>, false>()
   assertTypeEquality<BigintLessThan<-1234567890n, -1n>, true>()
+})
+it('distributes over union types', () => {
+  assertTypeEquality<BigintLessThan<5n, 1n | 9n>, boolean>()
+  assertTypeEquality<BigintLessThan<2n | 8n, 5n>, boolean>()
+  assertTypeEquality<BigintLessThan<1n | 2n, 3n | 4n>, true>()
+  assertTypeEquality<BigintLessThan<bigint, 3n | 4n>, boolean>()
+  assertTypeEquality<BigintLessThan<1n | 2n, bigint>, boolean>()
+  assertTypeEquality<BigintLessThan<bigint, bigint>, boolean>()
+  assertTypeEquality<BigintLessThan<any, 0n>, boolean>()
+  assertTypeEquality<BigintLessThan<0n, any>, boolean>()
+  assertTypeEquality<BigintLessThan<any, any>, boolean>()
 })
