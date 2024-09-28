283e449067d10e66f54389e5cf40002c07986a78
ootidea <ootidea@gmail.com>
2024-09-29 05:19:12 +0900

    test: add OverwriteProperties tests

--- src/object-manipulation/OverwriteProperties.ts
+++ src/object-manipulation/OverwriteProperties.ts
@@ -10,3 +10,18 @@ it('merges new properties', () => {
   assertTypeEquality<OverwriteProperties<{ a: 1; b: 2 }, { c: 9 }>, { a: 1; b: 2; c: 9 }>()
   assertTypeEquality<OverwriteProperties<{}, { a: 1 }>, { a: 1 }>()
 })
+it('keeps the readonly and optional modifiers', () => {
+  assertTypeEquality<OverwriteProperties<{ readonly a?: 1 }, { b: 2 }>, { readonly a?: 1; b: 2 }>()
+  assertTypeEquality<OverwriteProperties<{ a: 1 }, { readonly b?: 2 }>, { a: 1; readonly b?: 2 }>()
+  assertTypeEquality<OverwriteProperties<{ a: 1 }, { readonly a?: 'a' }>, { readonly a?: 'a' }>()
+})
+it('overwrite the readonly and optional modifiers', () => {
+  assertTypeEquality<OverwriteProperties<{ readonly a: 1 }, { a: 1 }>, { a: 1 }>()
+  assertTypeEquality<OverwriteProperties<{ a?: 1 }, { a: 1 }>, { a: 1 }>()
+})
+it('merges the index signature', () => {
+  assertTypeEquality<OverwriteProperties<{ a: 1 }, { [key: symbol]: boolean }>, { a: 1; [key: symbol]: boolean }>()
+  assertTypeEquality<OverwriteProperties<{ [key: symbol]: boolean }, { a: 1 }>, { [key: symbol]: boolean; a: 1 }>()
+  assertTypeEquality<OverwriteProperties<{ [key: symbol]: 1 }, { [key: symbol]: 'a' }>, { [key: symbol]: 'a' }>()
+  assertTypeEquality<OverwriteProperties<{ [key: symbol]: 1 }, { [key: string]: 'a' }>, { [key: symbol]: 1; [key: string]: 'a' }>()
+})
