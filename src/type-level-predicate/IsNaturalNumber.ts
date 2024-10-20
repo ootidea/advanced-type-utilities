4fb195ce00b8333d8cc6c1b8e59159da6cd3f899
ootidea <ootidea@gmail.com>
2024-10-20 18:25:41 +0900

    refactor: test case order

--- src/type-level-predicate/IsNaturalNumber.ts
+++ src/type-level-predicate/IsNaturalNumber.ts
@@ -22,16 +22,16 @@ it('returns true for natural numbers', () => {
   assertTypeEquality<IsNaturalNumber<1>, true>()
   assertTypeEquality<IsNaturalNumber<1e21>, true>()
 })
-it('returns false for non-natural numbers', () => {
-  assertTypeEquality<IsNaturalNumber<0.5>, false>()
-  assertTypeEquality<IsNaturalNumber<1e-21>, false>()
-  assertTypeEquality<IsNaturalNumber<Infinity>, false>()
-})
 it('returns false for negative numbers', () => {
   assertTypeEquality<IsNaturalNumber<-1>, false>()
   assertTypeEquality<IsNaturalNumber<-0.5>, false>()
   assertTypeEquality<IsNaturalNumber<-1e21>, false>()
   assertTypeEquality<IsNaturalNumber<NegativeInfinity>, false>()
+})
+it('returns false for non-integer numbers', () => {
+  assertTypeEquality<IsNaturalNumber<0.5>, false>()
+  assertTypeEquality<IsNaturalNumber<1e-21>, false>()
+  assertTypeEquality<IsNaturalNumber<Infinity>, false>()
 })
 it('distributes over union types', () => {
   assertTypeEquality<IsNaturalNumber<1 | 2>, true>()
