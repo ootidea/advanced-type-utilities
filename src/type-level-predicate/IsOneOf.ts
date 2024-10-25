70929bbbbf625b6f159876191c358ebe2e4dcd92
ootidea <ootidea@gmail.com>
2024-10-25 12:38:45 +0900

    feat: improve IsOneOf performance

--- src/type-level-predicate/IsOneOf.ts
+++ src/type-level-predicate/IsOneOf.ts
@@ -1,11 +1,14 @@
 import { assertTypeEquality } from '@/testUtilities'
 import type { Equals } from '@/type-level-predicate/Equals'
 
-export type IsOneOf<T, U extends readonly unknown[]> = U extends readonly [infer H, ...infer L]
-  ? Equals<T, H> extends true
-    ? true
-    : IsOneOf<T, L>
-  : false
+/**
+ * Determines whether a type is included in a tuple.
+ * Time complexity: O(n), but effectively O(1) due to lack of recursive calls.
+ * @example
+ * IsOneOf<string, [string, number]> // true
+ * IsOneOf<string, [number, bigint]> // false
+ */
+export type IsOneOf<T, U extends readonly unknown[]> = { [K in keyof U]: Equals<T, U[K]> }[number] extends false ? false : true
 
 assertTypeEquality<IsOneOf<string, [string, number]>, true>()
 assertTypeEquality<IsOneOf<string, [number, bigint]>, false>()
@@ -13,3 +16,8 @@ assertTypeEquality<IsOneOf<string, [any, unknown, never]>, false>()
 assertTypeEquality<IsOneOf<string, [string | number]>, false>()
 assertTypeEquality<IsOneOf<'text', [string]>, false>()
 assertTypeEquality<IsOneOf<string, []>, false>()
+
+assertTypeEquality<IsOneOf<string, [string, string]>, true>()
+assertTypeEquality<IsOneOf<string, string[]>, true>()
+assertTypeEquality<IsOneOf<2, [1, ...2[], 3]>, true>()
+assertTypeEquality<IsOneOf<3, [1, ...2[], 3]>, true>()
