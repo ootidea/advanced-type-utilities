3ebe70162f67c773b8a4bbd7267b0e9a16819e08
ootidea <ootidea@gmail.com>
2024-10-26 18:12:39 +0900

    fix: CopyArrayWritability any case

--- src/array-manipulation/CopyArrayWritability.ts
+++ src/array-manipulation/CopyArrayWritability.ts
@@ -1,11 +1,20 @@
 import type { Writable } from '@/object-manipulation/Writable'
-import { assertTypeEquality } from '@/testUtilities'
+import { assertTypeEquality, it } from '@/testUtilities'
+import type { Equals } from '@/type-level-predicate/Equals'
 
-export type CopyArrayWritability<T extends readonly unknown[], U extends readonly unknown[]> = T extends unknown[]
-  ? Writable<U>
-  : Readonly<U>
+export type CopyArrayWritability<From extends readonly unknown[], To extends readonly unknown[]> = Equals<From, any> extends true
+  ? To
+  : From extends unknown[]
+    ? Writable<To>
+    : Readonly<To>
 
-assertTypeEquality<CopyArrayWritability<readonly 1[], 2[]>, readonly 2[]>()
-assertTypeEquality<CopyArrayWritability<1[], readonly 2[]>, 2[]>()
-assertTypeEquality<CopyArrayWritability<1[], 2[]>, 2[]>()
-assertTypeEquality<CopyArrayWritability<readonly 1[], readonly 2[]>, readonly 2[]>()
+it('copies the writability from the first array to the second array', () => {
+  assertTypeEquality<CopyArrayWritability<readonly 1[], 2[]>, readonly 2[]>()
+  assertTypeEquality<CopyArrayWritability<1[], readonly 2[]>, 2[]>()
+  assertTypeEquality<CopyArrayWritability<1[], 2[]>, 2[]>()
+  assertTypeEquality<CopyArrayWritability<readonly 1[], readonly 2[]>, readonly 2[]>()
+})
+it('returns the second array as is if the first array is any', () => {
+  assertTypeEquality<CopyArrayWritability<any, 1[]>, 1[]>()
+  assertTypeEquality<CopyArrayWritability<any, readonly 1[]>, readonly 1[]>()
+})
