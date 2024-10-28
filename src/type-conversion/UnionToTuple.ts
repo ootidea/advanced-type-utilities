83c3a768260f734461cfd664e2bd1ae0dbe9638c
ootidea <ootidea@gmail.com>
2024-10-28 13:00:21 +0900

    fix: UnionToTuple<undefined | void>

--- src/type-conversion/UnionToTuple.ts
+++ src/type-conversion/UnionToTuple.ts
@@ -1,14 +1,17 @@
 import { assertTypeEquality } from '@/testUtilities'
 import type { UnionToIntersection } from '@/type-conversion/UnionToIntersection'
+import type { Equals } from '@/type-level-predicate/Equals'
 
-export type UnionToTuple<T> = UnionToIntersection<T extends T ? () => T : never> extends () => infer U
-  ? [...UnionToTuple<Exclude<T, U>>, U]
+export type UnionToTuple<T> = UnionToIntersection<T extends T ? () => [T] : never> extends () => [infer U]
+  ? [...UnionToTuple<T extends T ? (Equals<T, U> extends true ? never : T) : never>, U]
   : []
 
 assertTypeEquality<UnionToTuple<1 | 2 | 3>, [1, 2, 3]>()
+assertTypeEquality<UnionToTuple<undefined | void>, [undefined, void]>()
 assertTypeEquality<UnionToTuple<boolean>, [false, true]>()
 assertTypeEquality<UnionToTuple<keyof any>, [string, number, symbol]>()
 assertTypeEquality<UnionToTuple<number>, [number]>()
 assertTypeEquality<UnionToTuple<never>, []>()
 assertTypeEquality<UnionToTuple<[1?]>, [[1?]]>()
 assertTypeEquality<UnionToTuple<{ a?: 1 }>, [{ a?: 1 }]>()
+assertTypeEquality<UnionToTuple<string & {}>, [string & {}]>()
