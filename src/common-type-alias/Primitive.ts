3be5d593de0803eac96d2f54d5546afd2d33db67
ootidea <ootidea@gmail.com>
2024-11-02 07:34:22 +0900

    feat: add Primitive

---
+++ src/common-type-alias/Primitive.ts
@@ -0,0 +1,1 @@
+export type Primitive = boolean | number | bigint | string | symbol | null | undefined
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export type { JsonValue } from '@/common-type-alias/JsonValue'
 export type { NegativeInfinity } from '@/common-type-alias/NegativeInfinity'
 export type { NonNullish } from '@/common-type-alias/NonNullish'
 export type { nullish } from '@/common-type-alias/nullish'
+export type { Primitive } from '@/common-type-alias/Primitive'
 export type { TypedArray } from '@/common-type-alias/TypedArray'
 
 export type { LessThan } from '@/number-comparison/LessThan'
